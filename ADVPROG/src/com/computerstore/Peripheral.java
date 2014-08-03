86a9871da2628c04e61973b348c209525d49bfa7
Shaun Calagos <scalagos0123@live.com>
2014-08-04 07:54:23 +0800

    Computer Store Part II

--- ADVPROG/src/com/computerstore/Peripheral.java
+++ ADVPROG/src/com/computerstore/Peripheral.java
@@ -3,6 +3,10 @@ package com.computerstore;
 public class Peripheral extends ComputerPart {
 	private double Latency;
 	
+	public Peripheral() {
+		Mouse mouse = new Mouse();
+		Keyboard keyboard = new Keyboard();
+	}
 	public double getLatency() {
 		return Latency;
 	}
